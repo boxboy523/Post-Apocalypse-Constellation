3c4aaf1b3d08a2db967906b72650c9745e4554fe
Junyeong Kim <rlawnsdud523@gmail.com>
2026-07-03 19:59:23 +0900

    path movement

--- godot/player.gd
+++ godot/player.gd
@@ -1,25 +1,41 @@
 extends CharacterBody2D
 
+@export var path_choices: Array[PathFollow2D] = []
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+var idx = 0
+enum State {ON_PATH, TO_PATH, READY}
+var state = State.TO_PATH
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func follow(follower: PathFollow2D) -> void:
+	#get_parent().remove_child(self)
+	var new_follower = follower
+	reparent(new_follower, true)
+	state = State.TO_PATH
+	
+func _process(delta: float) -> void:
+	if not (get_parent() is PathFollow2D):
+		return
+	var follower := get_parent() as PathFollow2D
+	var path = follower.get_parent() as Path2D
+	print(str(position))
+	match state:
+		State.ON_PATH:
+			if follower and follower.progress_ratio < 1.0:
+				print(str(follower.progress_ratio))
+				follower.progress += SPEED * delta
+		State.TO_PATH:
+			position = position.move_toward(Vector2.ZERO, SPEED * delta)
+			if position == Vector2.ZERO:
+				state = State.READY
+		_:
+			pass
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("choose_path_1"):
+		follow(path_choices[0])
+	elif event.is_action_pressed("choose_path_2"):
+		follow(path_choices[1])
+	if event.is_action_pressed("start_path") and state == State.READY:
+		state = State.ON_PATH
+		
