49b865f89105a25077e57c22c91ecd19ee9f2849
Kim Jiseon <jisun1890@gmail.com>
2026-07-04 21:21:04 +0900

    feat: 표지판에 따른 경로 확률 조정

--- godot/scripts/obstacle_guide.gd
+++ godot/scripts/obstacle_guide.gd
@@ -1,14 +1,37 @@
 extends "res://scripts/obstacle_base.gd"
 
-@export_range(0.0, 1.0, 0.01)
-var guide_option_1_chance: float = 0.5
+@export var path_up_path: NodePath
+var new_path_up_weight: float
+var new_path_down_weight: float
+
+@export_enum("up", "down", "default")
+var direction: String = "default"
 
 func _on_area_entered(area: Area2D) -> void:
 	var player = area.get_parent()
 	if not player.is_in_group("player"):
 		return
-	print("표지판 확인")
-	if randf() < guide_option_1_chance:
-		print("1번 경로로 이동")
-	else:
-		print("2번 경로로 이동")
+	
+	var main := get_tree().current_scene
+	var path_up := main.get_node_or_null("MapBase/PathUp") as PathChoice
+	var path_down := main.get_node_or_null("MapBase/PathDown") as PathChoice
+
+	if path_up == null:
+		push_error("MapBase/PathUp을 찾지 못함")
+		return
+		
+	match direction:
+		"default":
+			new_path_up_weight = 1.0
+			new_path_down_weight = 1.0
+		"up":
+			new_path_up_weight = 1.0
+			new_path_down_weight = 0.0
+		"down":
+			new_path_up_weight = 0.0
+			new_path_down_weight = 1.0
+
+	path_up.prob_weight = new_path_up_weight
+	path_down.prob_weight = new_path_down_weight
+	print("PathUp prob_weight: ", path_up.prob_weight)
+	print("PathDown prob_weight: ", path_down.prob_weight)
