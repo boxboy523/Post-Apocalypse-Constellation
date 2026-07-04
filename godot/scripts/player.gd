dbc9423a5ec875704fd606301f3fe9f1e1267fd2
Junyeong Kim <rlawnsdud523@gmail.com>
2026-07-04 10:50:20 +0900

    add comments in player.gd

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -18,16 +18,15 @@ func follow(path: PathChoice) -> void:
 	reparent(new_follower, true)
 	$Camera2D.reset_smoothing()
 	state = PlayerState.TO_PATH
-	
+
 func _process(delta: float) -> void:
 	if not (get_parent() is PathFollow2D):
 		return
 	var follower := get_parent() as PathFollow2D
 	print(str(state))
 	match state:
-		PlayerState.ON_PATH:
-			print(str(follower.progress_ratio))
-			if not new_map:
+		PlayerState.ON_PATH: # 플레이어가 경로를 따라 이동 중
+			if not new_map: # 경로 이동 직전에 도착점 뒤에 다음 맵 생성
 				var path := follower.get_parent() as PathChoice
 				var last_pos = path.global_transform * path.curve.get_point_position(path.curve.get_point_count() - 1)
 				if path.next_set:
@@ -35,24 +34,23 @@ func _process(delta: float) -> void:
 					get_tree().current_scene.add_child(new_map)
 					new_map.position = last_pos
 			if follower and follower.progress_ratio < 1.0:
-				follower.progress += SPEED * delta
-			elif new_map:
+				follower.progress += SPEED * delta # 경로를 따라 이동
+			elif new_map: # 경로 끝에 도착했을 때 다음 맵으로 이동
 				path_choices = new_map.get_paths()
 				state = PlayerState.TO_PATH
 				map = new_map
 				new_map = null
-		PlayerState.TO_PATH:
+		PlayerState.TO_PATH: # 플레이어가 경로의 시작점으로 이동 중
 			position = position.move_toward(Vector2.ZERO, SPEED * delta)
 			if position == Vector2.ZERO:
 				state = PlayerState.READY
 		_:
 			pass
-	
+
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("choose_path_1"):
+	if event.is_action_pressed("choose_path_1"): # 경로 1번
 		follow(path_choices[0])
-	elif event.is_action_pressed("choose_path_2"):
+	elif event.is_action_pressed("choose_path_2"): # 경로 2번
 		follow(path_choices[1])
-	if event.is_action_pressed("start_path") and state == PlayerState.READY:
+	if event.is_action_pressed("start_path") and state == PlayerState.READY: # 이동 시작
 		state = PlayerState.ON_PATH
-		
