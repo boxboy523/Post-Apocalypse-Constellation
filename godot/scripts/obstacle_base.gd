e2550d68a28981bddc1757a9b02f066fa217a9b7
Kim Jiseon <jisun1890@gmail.com>
2026-07-04 11:41:44 +0900

    feat: 캐릭터 부상 상호작용 추가
    캐릭터 체력 추가
    부상 장애물 추가
    적과 조우시 무기에 따른 도주 확률 추가

--- godot/scripts/obstacle_base.gd
+++ godot/scripts/obstacle_base.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export_enum("default", "hurt", "enemy", "guide")
-var obstacle_type: String = "hurt"
+var obstacle_type: String = "enemy"
 
 @export var damage: int = 1
 @export_range(0.0, 1.0, 0.01)
@@ -11,16 +11,21 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
+	
+	var player = area.get_parent()
+		
 	match obstacle_type:
 		"default":
 			print("충돌")
 
 		"hurt":
 			print("부상 입음")
-			print("대미지: ", damage)
+			player.take_damage(damage)
 
 		"enemy":
 			print("적과 조우")
+			var success: bool = player.run_from_enemy(damage)
+			queue_free()
 
 		"guide":
 			print("표지판에 도달")
