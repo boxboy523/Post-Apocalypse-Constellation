a29e6ff5634f40cb4694959127fe1c7f49f1b124
Junyeong Kim <rlawnsdud523@gmail.com>
2026-07-04 17:01:16 +0900

    add player movement

--- godot/scripts/path_choice.gd
+++ godot/scripts/path_choice.gd
@@ -3,15 +3,24 @@ extends Path2D
 @export var desc: String
 @export var next_set: PackedScene
 @export var next_paths: Array[PathChoice] = []
+@export var is_primary: bool = false # 이 경로가 씬에 존재할경우 자동으로 로드될지 여부
 
 var map: ChoiceMap
+
+func _init() -> void:
+	var line = Line2D.new()
+	line.points = curve.get_baked_points()
+	line.width = 2.0
+	line.default_color = Color(0.0, 1.0, 0.0, 1.0)
+	add_child(line)
+
 
 func instantiate_map() -> void:
 	if next_set:
 		map = next_set.instantiate() as ChoiceMap
-		var pos = self.curve.get_point_position(self.curve.get_point_count() - 1)
+		var pos = self.to_global(self.curve.get_point_position(self.curve.get_point_count() - 1))
 		get_tree().current_scene.add_child(map)
-		map.position = pos
+		map.global_position = pos
 		next_paths.append_array(map.get_paths())
 	else:
 		push_error("No next_set defined for PathChoice: " + str(self))
--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -1,53 +1,55 @@
 extends Node2D
 
 @export var map: ChoiceMap
-var new_map: ChoiceMap
 
 var path_choices: Array[PathChoice]
+var current_path: PathChoice
 const SPEED = 200.0
 var idx = 0
-enum PlayerState {ON_PATH, TO_PATH, READY}
-var state = PlayerState.TO_PATH
+enum PlayerState {ON_PATH, TO_PATH, READY, FINISHED, END}
+var state = PlayerState.FINISHED
 
 func _ready() -> void:
 	path_choices = map.get_paths()
 
 func follow(path: PathChoice) -> void:
+	var pos = global_position
+	current_path = path
 	reparent(path, true)
-	$Camera2D.reset_smoothing()
+	global_position = pos
+	$"LerfFollow/Camera2D".reset_smoothing()
+	self.progress = 0.0
 	state = PlayerState.TO_PATH
 
 func _process(delta: float) -> void:
 	if not (get_parent() is PathChoice):
 		return
-	var path := get_parent() as PathChoice
 	match state:
 		PlayerState.ON_PATH: # 플레이어가 경로를 따라 이동 중
-			if not new_map: # 경로 이동 직전에 도착점 뒤에 다음 맵 생성
-				var last_pos = path.global_transform * path.curve.get_point_position(path.curve.get_point_count() - 1)
-				if path.next_set:
-					new_map = path.next_set.instantiate() as ChoiceMap
-					get_tree().current_scene.add_child(new_map)
-					new_map.position = last_pos
+			if current_path.next_set and not current_path.map: # 다음 맵 생성
+				current_path.instantiate_map()
 			if self.progress_ratio < 1.0:
 				self.progress += SPEED * delta # 경로를 따라 이동
-			elif new_map: # 경로 끝에 도착했을 때 다음 맵으로 이동
-				path_choices = new_map.get_paths()
-				state = PlayerState.TO_PATH
-				follow(path_choices[0])
-				map = new_map
-				new_map = null
+			else:
+				path_choices = current_path.get_paths() # 다음 경로 로드
+				if path_choices.size() == 0:
+					state = PlayerState.END
+				else:
+					follow(path_choices[0])
+					state = PlayerState.FINISHED
 		PlayerState.TO_PATH: # 플레이어가 경로의 시작점으로 이동 중
-			position = position.move_toward(path.curve.get_point_position(0), SPEED * delta)
-			if position == path.curve.get_point_position(0):
+			position = position.move_toward(current_path.curve.get_point_position(0), SPEED * delta)
+			if position == current_path.curve.get_point_position(0):
 				state = PlayerState.READY
 		_:
 			pass
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("choose_path_1"): # 경로 1번
-		follow(path_choices[0])
-	elif event.is_action_pressed("choose_path_2"): # 경로 2번
-		follow(path_choices[1])
+	if state == PlayerState.FINISHED:
+		if event.is_action_pressed("choose_path_1"): # 경로 1번
+			follow(path_choices[0])
+		elif event.is_action_pressed("choose_path_2"): # 경로 2번
+			follow(path_choices[1])
+		state = PlayerState.TO_PATH
 	if event.is_action_pressed("start_path") and state == PlayerState.READY: # 이동 시작
 		state = PlayerState.ON_PATH
